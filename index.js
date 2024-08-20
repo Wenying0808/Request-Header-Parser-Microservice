f81c2a04c88ef0fdc9600cabb38568ee1be1e70a
Wenying Chang <changwenyingnl@gmail.com>
2024-08-20 18:47:19 +0200

    install dotenv and add microservice

--- index.js
+++ index.js
@@ -20,8 +20,17 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const headers = req.headers;
+  const ipaddress = headers.host;
+  const language = headers['accept-language'];
+  const software = headers['user-agent'];
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software,
+  });
 });
 
 // listen for requests :)
